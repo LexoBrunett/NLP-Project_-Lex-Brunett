7c6f195de7afd6a2f0c1894c91be70db32959f70
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2024-11-10 21:01:37 +0000

    model

--- src/app.py
+++ src/app.py
@@ -1,4 +1,124 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+import pandas as pd
+
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/NLP-project-tutorial/main/url_spam.csv")
+total_data.head()
+
+
+total_data["is_spam"] = total_data["is_spam"].apply(lambda x: 1 if x else 0).astype(int)
+total_data.head()
+
+print(total_data.shape)
+total_data = total_data.drop_duplicates()
+total_data = total_data.reset_index(inplace = False, drop = True)
+total_data.shape
+
+print(f"Spam: {len(total_data.loc[total_data.is_spam == 1])}")
+print(f"No spam: {len(total_data.loc[total_data.is_spam == 0])}")
+
+import regex as re
+
+def preprocess_text(text):
+    # Remove any character that is not a letter (a-z) or white space ( )
+    text = re.sub(r'[^a-z ]', " ", text)
+    
+    # Remove white spaces
+    text = re.sub(r'\s+[a-zA-Z]\s+', " ", text)
+    text = re.sub(r'\^[a-zA-Z]\s+', " ", text)
+
+    # Multiple white spaces into one
+    text = re.sub(r'\s+', " ", text.lower())
+
+    # Remove tags
+    text = re.sub("&lt;/?.*?&gt;"," &lt;&gt; ", text)
+
+    return text.split()
+
+total_data["url"] = total_data["url"].apply(preprocess_text)
+total_data.head()
+
+from nltk import download
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+download("wordnet")
+lemmatizer = WordNetLemmatizer()
+
+download("stopwords")
+stop_words = stopwords.words("english")
+
+def lemmatize_text(words, lemmatizer = lemmatizer):
+    tokens = [lemmatizer.lemmatize(word) for word in words]
+    tokens = [word for word in tokens if word not in stop_words]
+    tokens = [word for word in tokens if len(word) > 3]
+    return tokens
+
+total_data["url"] = total_data["url"].apply(lemmatize_text)
+total_data.head()
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+wordcloud = WordCloud(width = 800, height = 800, background_color = "black", max_words = 1000, min_font_size = 20, random_state = 42)\
+    .generate(str(total_data["url"]))
+
+fig = plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+tokens_list = total_data["url"]
+tokens_list = [" ".join(tokens) for tokens in tokens_list]
+
+vectorizer = TfidfVectorizer(max_features = 5000, max_df = 0.8, min_df = 5)
+X = vectorizer.fit_transform(tokens_list).toarray()
+y = total_data["is_spam"]
+
+X[:5]
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+from sklearn.svm import SVC
+
+model = SVC(kernel = "linear", random_state = 42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+y_pred
+
+
+from sklearn.metrics import accuracy_score
+
+accuracy_score(y_test, y_pred)
+
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+
+hyperparams = {
+    "C": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+    "kernel": ["linear", "poly", "rbf", "sigmoid", "precomputed’"],
+    "degree": [1, 2, 3, 4, 5],
+    "gamma": ["scale", "auto"]
+}
+
+# We initialize the random search
+grid = GridSearchCV(model, hyperparams, scoring = "accuracy", cv = 5)
+grid
+
+grid.fit(X_train, y_train)
+
+print(f"Best hyperparameters: {grid.best_params_}")
+
+opt_model = SVC(C = 1000, degree = 1, gamma = "auto", kernel = "poly", random_state = 42)
+opt_model.fit(X_train, y_train)
+y_pred = opt_model.predict(X_test)
+accuracy_score(y_test, y_pred)
+
+from pickle import dump
+
+dump(model, open("/workspaces/NLP-Project_-Lex-Brunett/models/svm_classifier_C-1000_deg-1_gam-auto_ker-poly_42.sav", "wb"))
